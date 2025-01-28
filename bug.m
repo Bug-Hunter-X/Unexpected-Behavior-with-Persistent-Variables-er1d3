11670c93b6d90dd756e3175e770f73ea6ae76d87
BugHunter-X <askzenai@gmail.com>
2025-01-28 07:14:04 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,9 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  result = input * 2; 
+end
+
+% Example of how to use the function
+input = 10;
+result = myFunction(input);
+disp(result);
